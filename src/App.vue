be06629adf608dd8e6ca7299cf24381a0854b975
mt-dafonte <mteresa.delafuente.c@gmail.com>
2025-11-21 23:07:47 -0300

    Implementación con BootstrapVue (bootstrap-vue-3)

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import EjemploBoostrapVue  from './components/EjemploBoostrapVue.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1 class="text-center">Bootstrap Vue</h1>
+  <EjemploBoostrapVue/>
 </template>
 
 <style scoped>
---
+++ src/components/EjemploBoostrapVue.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: 'EjemploBootstrapVue',
+  data() {
+    return {
+      nombre: '',
+      mensaje: '',
+    }
+  },
+  methods: {
+    saludar() {
+      this.mensaje = `Hola, ${this.nombre || 'visitante'} 👋`
+    }
+  }
+}
+</script>
+
+<template>
+  <b-container class="mt-4">
+    <b-row class="justify-content-center">
+      <b-col cols="12" md="6">
+
+        <b-card title="Formulario de saludo" class="text-center">
+
+          <b-form-input
+            v-model="nombre"
+            placeholder="Escribe tu nombre"
+            class="mb-3"
+          />
+
+          <b-button variant="primary" @click="saludar">
+            Saludar
+          </b-button>
+
+          <hr>
+
+          <p class="mt-3" v-if="mensaje">
+            {{ mensaje }}
+          </p>
+
+        </b-card>
+
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
